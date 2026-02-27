03ec5d40c14e0c019581c1366425b5ffb1995ea6
gadgeski <xtwh.ce5bil@gmail.com>
2026-02-27 23:46:09 +0900

    fix: correct package name in ExampleInstrumentedTest

--- app/src/androidTest/java/com/gadgeski/injector/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/gadgeski/injector/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.injector", appContext.packageName)
+        assertEquals("com.gadgeski.injector", appContext.packageName)
     }
 }
